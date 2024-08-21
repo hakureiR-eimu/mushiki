91d705aba9f704ba80e60716257fb1ec7d52f266
CodeOfUsami <zhang0285@foxmail.com>
2024-08-21 21:08:57 +0800

    feat(Scripts\UIFramework):add Base&&BasePanel

---
+++ Assets/Scripts/UIFramework/Base/BasePanel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 所有UI面板的父类，包含UI面板的状态信息
+/// </summary>
+public class BasePanel
+{
+    public UIType UIType { get; private set; }
+
+    /// <summary>
+    /// UI进入时执行的操作，只会执行一次
+    /// </summary>
+    public virtual void OnEnter(){ }
+
+    /// <summary>
+    /// UI暂停时执行的操作
+    /// </summary>
+    public virtual void OnPause() { }
+
+    /// <summary>
+    /// UI继续时执行的操作
+    /// </summary>
+    public virtual void OnResume() { }
+
+    /// <summary>
+    /// UI退出时执行的操作
+    /// </summary>
+    public virtual void OnExit() { }
+}
